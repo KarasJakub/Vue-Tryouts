3f29d4fab331188d0a16d4099d11cb1107b33d0b
Jakub Karaś <jakub.karas@onet.pl>
2023-05-30 18:57:16 +0200

    Created Header and added Button component

---
+++ src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <AddButton text="Add Task" color="green"/>
+    </header>
+</template>
+
+<script>
+import AddButton from "./Button.vue"
+
+    export default {
+        name: "MainHeader",
+        props: {
+            title: String
+        },
+        components: {
+            AddButton
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
